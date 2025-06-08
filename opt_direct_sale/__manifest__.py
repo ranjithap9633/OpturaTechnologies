727dc953cd595182a1b3b61ae5652cab66ab44d0
admin <admin>
2025-06-08 12:38:32 +0530

    [test]

--- opt_direct_sale/__manifest__.py
+++ opt_direct_sale/__manifest__.py
@@ -10,6 +10,8 @@
     'website': 'https://opturatech.com',
     'category': 'Sales',
     'depends': ['sale_management','account','stock'],
+    'price': '1.0'
+    'currency': 'USD',
     'data': [
         # Add your data files here, e.g. 'views/sale_order_views.xml'
     ],
